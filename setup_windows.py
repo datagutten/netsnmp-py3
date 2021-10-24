280955409f2383af570f70a8e1127d17e33b411b
datagutten <datagutten@datagutten.net>
2021-10-24 21:37:55 +0200

    Correct dll path

--- setup_windows.py
+++ setup_windows.py
@@ -82,7 +82,7 @@ setup(
                  libraries=libs)
     ],
     include_package_data=platform.system() == 'Windows',
-    package_data={"": ['*.dll']},
+    package_data={"": ['dll/*.dll']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
